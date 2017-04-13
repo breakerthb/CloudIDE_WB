e6491573cc667b8d2ee4987048afedb88d9d4952
breakerthb <breakerthb@126.com>
2017-04-13 08:06:02 +0000

    Add PicBed rename script

---
+++ PicBed/rename.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+OldName="prt.png"
+
+# Check paramters number
+if [ $# != 0 ] ; then
+    OldName=$1
+fi 
+
+# Get current year
+Year=`date +%Y`
+
+# Check whether the file exist
+if [ ! -f "$OldName" ]; then
+    echo "File $OldName is not exist!"
+    exit 0
+fi 
+
+LinkPath="https://raw.githubusercontent.com/breakerthb/CloudIDE_WB/master/PicBed/$Year"
+
+# Get new file name
+NewName=`date +%s`
+NewName="$NewName.png"
+
+echo $NewName
+
+mv prt.png $NewName
+mv $NewName $Year/$NewName
+
+echo "NewPicName:"
+echo "$LinkPath$NewName"
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Cloud Work Bench
 
-Do daily github operate space
+# HexoBlog
+
+To manage hexo blog
+
+# PicBed
+
+Use this folder as a picture bed
+
+## How to use
+
+- Upload picture file
+
+The default file name is "ptr.png"
+
+- Rename file
+
+Rename file name with script : rename.sh
+
+    $ sh rename.sh
+    $ sh rename.sh sourcepic.png
+ 
+The url will be printed on the screen
+    
+- Commit & Push
+
+Submit the new files into repository
+
+
